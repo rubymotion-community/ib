4c18c19d12ef19b6fd06706fa2ab24918309f0cf
Yury Korolev <yury.korolev@gmail.com>
2012-07-13 03:37:10 +0400

    Include frameworks

--- lib/ib/project.rb
+++ lib/ib/project.rb
@@ -1,22 +1,35 @@
 class IB::Project
-  def write app_path = "./app", resources_path = "./resources", pods_headers = "./vendor/Pods/Headers"
+  def write app_path = "app", resources_path = "resources", pods_headers_path = "vendor/Pods/Headers"
     project = Xcodeproj::Project.new
-    target = project.targets.new_static_library(:ios, 'design')
+    target = project.targets.new_static_library(:ios, 'ui')
 
     resources = project.groups.new('path' => resources_path, 'name' => 'Resources')
-    support = project.groups.new('name' => 'Support Files')
-    pods = project.groups.new('name' => 'Pods')
+    support   = project.groups.new('name' => 'Supporting Files')
+    pods      = project.groups.new('name' => 'Pods')
 
+    stubs_path = "ui.xcodeproj/stubs.h"
     IB::Generator.new.write(app_path, "ui.xcodeproj")
-    support.files.new 'path' => "ui.xcodeproj/stubs.h"
+    support.files.new 'path' => stubs_path
 
-    Dir.glob("#{resources_path}/**/*.{xcdatamodeld,png,jpg,jpeg,storyboard,xib}") do |file|
+    resource_exts = %W{xcdatamodeld png jpg jpeg storyboard xib lproj}
+    Dir.glob("#{resources_path}/**/*.{#{resource_exts.join(",")}}") do |file|
       resources.files.new('path' => file)
     end
 
-    Dir.glob("#{pods_headers}/**/*.h") do |file|
+    Dir.glob("#{pods_headers_path}/**/*.h") do |file|
       pods.files.new('path' => file)
     end
+
+    # Add Basic System Frameworks
+    # frameworks = ["UIKit", "QuartzCore", "CoreGraphics", "CoreData"]
+    # doesn't work with UIKit
+
+    frameworks = ["QuartzCore", "CoreGraphics", "CoreData"]
+    frameworks.each do |framework|
+      file = project.add_system_framework framework
+      target.frameworks_build_phases.first << file
+    end
+
 
     project.save_as("ui.xcodeproj")
   end
