b5814230163ff30df14f20d4d0ebadeb090f2417
Yury Korolev <yury.korolev@gmail.com>
2012-07-12 23:59:22 +0400

    Close #4. Put stubs.h in "Support Files" group and all resources in Resources group.

--- lib/ib/project.rb
+++ lib/ib/project.rb
@@ -1,17 +1,17 @@
 class IB::Project
   def write app_path = "./app", resources_path = "./resources"
     project = Xcodeproj::Project.new
-    target = project.targets.new_static_library(:ios, 'ui')
-    stubs = IB::Generator.new.write(app_path, "ui.xcodeproj")
-    stubs_path = Pathname.new("ui.xcodeproj/stubs.h")
-    files = [Xcodeproj::Project::PBXNativeTarget::SourceFileDescription.new(stubs_path, nil, nil)]
-    
+    target = project.targets.new_static_library(:ios, 'design')
+
+    resources = project.groups.new('path' => resources_path, 'name' => 'Resources')
+    support = project.groups.new('name' => 'Support Files')
+
+    IB::Generator.new.write(app_path, "ui.xcodeproj")
+    support.files.new 'path' => "ui.xcodeproj/stubs.h"
 
     Dir.glob("#{resources_path}/**/*.{xcdatamodeld,png,jpg,jpeg,storyboard,xib}") do |file|
-      files << Xcodeproj::Project::PBXNativeTarget::SourceFileDescription.new(Pathname.new(file), nil, nil)
+      resources.files.new('path' => file)
     end
-
-    target.add_source_files(files)
 
     project.save_as("ui.xcodeproj")
   end
