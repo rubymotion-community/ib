1b192b11b5b919ef02c6a3742f00c7a791b5ca0f
yury <yury.korolev@gmail.com>
2012-11-16 15:54:11 +0400

    Remove depricated methods

--- lib/ib/outlets.rb
+++ lib/ib/outlets.rb
@@ -3,21 +3,11 @@ module IB
   def ib_outlet name, type = "id"
   end
 
-  def outlet_accessor name, type = "id"
-    puts "Depricated: use `outlet` instead of `outlet_accessor`"
-    attr_accessor name
-  end
-
   def outlet name, type = "id"
     attr_accessor name
   end
 
   def ib_outlet_collection name, type = "id"
-  end
-
-  def outlet_collection_accessor name, type = "id"
-    puts "Depricated: use `outlet_collection` instead of `outlet_collection_accessor`"
-    attr_accessor name
   end
 
   def outlet_collection name, type = "id"
