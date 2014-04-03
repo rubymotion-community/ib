b2d87c94cd72d77a948756cf391111dcfaf619bb
Ivan Malijkh <i.malijkh@gmail.com>
2014-04-03 23:00:48 +0200

    restructure project test

--- spec/lib/ib/project_spec.rb
+++ spec/lib/ib/project_spec.rb
@@ -3,21 +3,27 @@ require "spec_helper"
 require "ib/project"
 
 describe IB::Project do
-  it "generates xcode project" do
-    IB::Project.new(app_path:"spec/fixtures/common").write()
+  it "defines #write" do
+    expect(IB::Project.new(app_path: 'spec/fixtures/common')).to respond_to :write
   end
 
-  it "app_files" do
-    Motion::Project::App.config.stub('files').and_return(
-      [
-        '/usr/local/xxx/gems/xxx/lib/xxxx.rb',
-        'User/xxx/gems/xxx/lib/motion/xxxx.rb',
-        './app/aaa.rb',
-        './app/controllers/bbb.rb',
-      ]
-    )
-    expect(IB::Project.new(app_path:"spec/fixtures/common").app_files).to eq(
-      ['./app/aaa.rb', './app/controllers/bbb.rb',]
-    )
+  it "generates xcode project" do
+    project = IB::Project.new(app_path: 'spec/fixtures/common')
+    project.write
+  end
+
+  describe '#app_files' do
+    it "returns a list of files under app directory of the project" do
+      Motion::Project::App.config.stub('files').and_return(
+        [
+          '/usr/local/xxx/gems/xxx/lib/xxxx.rb',
+          'User/xxx/gems/xxx/lib/motion/xxxx.rb',
+          './app/aaa.rb',
+          './app/controllers/bbb.rb',
+        ]
+      )
+      project = IB::Project.new(app_path: 'spec/fixtures/common')
+      expect(project.app_files).to eq ['./app/aaa.rb', './app/controllers/bbb.rb']
+    end
   end
 end
