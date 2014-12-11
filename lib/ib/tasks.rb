15c3b50e18617b48247a4c68c7d60978c75dafb4
Eloy Durán <eloy.de.enige@gmail.com>
2014-12-11 18:12:36 +0100

    [Rake] Allow user to customize the project configuration.

--- lib/ib/tasks.rb
+++ lib/ib/tasks.rb
@@ -1,15 +1,31 @@
 # -*- encoding : utf-8 -*-
 require 'rake' unless defined? Rake
 
-namespace :ib do
-  desc "Generates ib.xcodeproj and opens it in XCode"
-  task :open => :project do
-    system "open ib.xcodeproj"
-  end
+module IB
+  class RakeTask
+    include Rake::DSL
 
-  desc "Generates ib.xcodeproj"
-  task :project do
-    IB::Project.new.write
+    def initialize
+      require 'ib/project'
+      @project = IB::Project.new
+      yield @project if block_given?
+      define_tasks
+    end
+
+    def define_tasks
+      namespace :ib do
+        desc "Generates ib.xcodeproj"
+        task :project do
+          @project.write
+        end
+
+        desc "Generates ib.xcodeproj and opens it in XCode"
+        task :open => :project do
+          system "open ib.xcodeproj"
+        end
+      end
+      desc "Same as 'ib:open'"
+      task :ib => "ib:open"
+    end
   end
 end
-task :ib => "ib:open"
